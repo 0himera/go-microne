e75b80b0633cb3ccccbd9ed1ce3a6c44e7e4dae2
amir <h1mera@icloud.com>
2026-04-25 21:17:02 +0300

    register datetime handler

--- shell/server/http_handler.go
+++ shell/server/http_handler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/0himera/go-microne/internal/domains"
 	"github.com/0himera/go-microne/internal/infra"
 	"github.com/0himera/go-microne/internal/protocol/httpapi"
 )
@@ -10,7 +11,11 @@ import (
 func buildHTTPHandler() http.Handler {
 	idGen:= infra.UUIDGenerator{}
 
+	clock := infra.SystemClock{}
+	systemCloclService := domains.NewDatetimeService(clock)
+
 	mux:= http.NewServeMux()
+	mux.Handle("GET /", httpapi.CreateDatetimeHandler(systemCloclService))
 
 	var handler http.Handler = mux
 	handler = httpapi.RequestIDMiddleware(idGen)(handler)
